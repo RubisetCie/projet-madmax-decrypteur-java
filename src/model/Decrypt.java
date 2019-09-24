d3003515a2f6ed6736e04ceab6880a0ad7c48068
rubisetcie <rubisetcie@gmail.com>
2019-09-24 10:20:32 +0200

    Ajout des fonctionnalités de décryptage.

--- src/model/Decrypt.java
+++ src/model/Decrypt.java
@@ -13,6 +13,24 @@ public class Decrypt
      */
     public String decrypt(String data, String key)
     {
-        return "";
+        // Buffer pour le string de sortie :
+        char[] outputString = new char[data.length()];
+        
+        // Variables pour itérer au travers la clef :
+        int key_i = 0;
+        
+        for (int i = 0; i < data.length(); i++)
+        {
+            outputString[i] = (char)(data.charAt(i) ^ key.charAt(key_i));
+            
+            // On décale dans la clef :
+            if (key_i < key.length()-1)
+                key_i++;
+            else
+                key_i = 0;
+        }
+        
+        // Enfin, on retourne la chaîne de caractère chiffrée :
+        return new String(outputString);
     }
 }
